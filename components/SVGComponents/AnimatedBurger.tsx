7cdcdfce04a157b0cab09625b967271721ca6070
Yashwant Ostwal <140978232+YashwantOstwal@users.noreply.github.com>
2025-05-05 04:21:02 +0530

    Bug fixes

--- components/SVGComponents/AnimatedBurger.tsx
+++ components/SVGComponents/AnimatedBurger.tsx
@@ -6,6 +6,14 @@ import * as motion from "motion/react-client";
 export default function AnimatedBurger(
   props: MotionProps & { className?: string },
 ) {
+  // const childVariants = {
+  //   initial: {
+  //     x: "0%",
+  //   },
+  //   whileHover: {
+  //     x: "100%",
+  //   },
+  // };
   const childVariants = {
     initial: {
       strokeDashoffset: 0,
@@ -16,36 +24,43 @@ export default function AnimatedBurger(
   };
   return (
     <>
-      <motion.svg
-        className={cn("h-4 w-7", props.className)}
-        viewBox="0 0 100 100"
-        preserveAspectRatio="none"
+      <motion.div
+        className={cn("flex h-4 w-7 flex-col justify-between", props.className)}
       >
-        <motion.path
-          variants={childVariants}
-          transition={{ duration: 0.5, delay: 0 }}
-          d="M3 2H99.5"
-          strokeWidth="4px"
-          strokeLinecap="round"
-          strokeDasharray="100 75"
-        />
-        <motion.path
-          variants={childVariants}
-          transition={{ duration: 0.5, delay: 0.1 }}
-          d="M3 47H99.5"
-          strokeWidth="4px"
-          strokeLinecap="round"
-          strokeDasharray="100 75"
-        />
-        <motion.path
-          variants={childVariants}
-          transition={{ duration: 0.5, delay: 0.2 }}
-          d="M3 95H99.5"
-          strokeWidth="4px"
-          strokeLinecap="round"
-          strokeDasharray="100 75"
-        />
-      </motion.svg>
+        <svg preserveAspectRatio="none" viewBox="0 0 100 1">
+          <motion.path
+            d="M0 0H100"
+            variants={childVariants}
+            transition={{ duration: 0.5, delay: 0 }}
+            className="bg-white"
+            strokeWidth="4px"
+            strokeDasharray="100 75"
+            strokeLinecap="round"
+          />
+        </svg>
+        <svg preserveAspectRatio="none" viewBox="0 0 100 1">
+          <motion.path
+            d="M0 0H100"
+            variants={childVariants}
+            transition={{ duration: 0.5, delay: 0.1 }}
+            className="bg-white"
+            strokeWidth="4px"
+            strokeDasharray="100 75"
+            strokeLinecap="round"
+          />
+        </svg>
+        <svg preserveAspectRatio="none" viewBox="0 0 100 1">
+          <motion.path
+            d="M0 0H100"
+            variants={childVariants}
+            transition={{ duration: 0.5, delay: 0.2 }}
+            className="bg-white"
+            strokeWidth="4px"
+            strokeDasharray="100 75"
+            strokeLinecap="round"
+          />
+        </svg>
+      </motion.div>
     </>
   );
 }
